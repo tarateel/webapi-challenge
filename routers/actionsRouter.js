090e8707b31c7ee6a159db9b04a0c81eabcebb4c
Tara Sherman <taradelin@gmail.com>
2019-12-14 13:46:47 -0500

    Created GET request to fetch actions on a project

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -10,6 +10,27 @@ const router = express.Router({
   mergeParams: true
 });
 
+// getProjectActions() takes a project id as it's only argument and returns a list of all the actions for the project.
+// project_id	number	required, must be the id of an existing project.
+// description	string	up to 128 characters long, required.
+// notes	string	no size limit, required. Used to record additional notes or requirements to complete the action.
+// completed	boolean	used to indicate if the action has been completed, not required
+router.get('/', (req, res) => {
+  // request actions by ID parameter
+  projects.getProjectActions(req.params.id)
+    .then(actions => {
+      // return 'OK' code and the actions on the specified project
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      // if an error, respond with 'server error' code and json error message
+      res.status(500).json({
+        err: err,
+        errorMessage: 'The actions on this project could not be retrieved.'
+      });
+    });
+});
+
 
 
 module.exports = router;
